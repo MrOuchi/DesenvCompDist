47a338026d95e92c87f45253b4f2b8b579914ef9
MrOuchi <mrwolfzero@gmail.com>
2021-04-27 11:25:18 -0300

    Tarefas27/04/2021
    Tarefas 7, 8 e 9

---
+++ Tarefa7&8OAuth/Tarefa7/src/main/java/com/example/Tarefa7/Tarefa7Application.java
@@ -0,0 +1,13 @@
+package com.example.Tarefa7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarefa7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarefa7Application.class, args);
+	}
+
+}
---
+++ Tarefa7&8OAuth/Tarefa7/src/test/java/com/example/Tarefa7/Tarefa7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tarefa7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarefa7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
